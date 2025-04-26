832a2b4f509db01c280b22ee168cd4d0670da510
Angela Omondi <omondiangela426@gmail.com>
2025-04-26 12:38:11 +0300

    updating code with explanation comments

--- exercise4.py
+++ exercise4.py
@@ -1,10 +1,12 @@
 def reverse_string(string1):
+#it stores the reversed version of string 1
     reversed_string = ""
-
+#each character in the string is reversed from left to right
     for char in string1:
       reversed_string = char + reversed_string
     return reversed_string
 
 string1 = input("Enter a string value: ")
+#the function reverse_string is being called
 string2 = reverse_string(string1)
 print ("The reversed string value is", string2)
