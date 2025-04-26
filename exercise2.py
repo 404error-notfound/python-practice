18d453d3c45f9d072c2e1e019d709130041e448e
Angela Omondi <omondiangela426@gmail.com>
2025-04-26 08:54:27 +0300

    code that checks if an integer is even or odd

---
+++ exercise2.py
@@ -0,0 +1,8 @@
+#n is an integer number
+n = int(input ("Enter any number: "))
+remainder = n % 2
+if(remainder ==0):
+    print(n, " is an Even number")
+else:
+    print(n, " is an Odd number")
+    
