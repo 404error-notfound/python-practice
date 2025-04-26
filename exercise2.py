7346a2f71fd96e1a392411bd4d375b64ea5920d7
Angela Omondi <omondiangela426@gmail.com>
2025-04-26 10:12:16 +0300

    explaining the use of modulus

--- exercise2.py
+++ exercise2.py
@@ -1,5 +1,6 @@
 #n is an integer number
 n = int(input ("Enter any number: "))
+# modulus (%) in this particular code shows the remainder of a number that has been divided by 2
 remainder = n % 2
 if(remainder ==0):
     print(n, " is an Even number")
